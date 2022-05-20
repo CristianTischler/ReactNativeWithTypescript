18e7a84f3e5edb4f95cdf56a5393089febe74225
CristianTischler <91978480+CristianTischler@users.noreply.github.com>
2022-05-20 14:49:40 -0300

    feat: renegando

--- Practica 01/src/components/Usuarios.tsx
+++ Practica 01/src/components/Usuarios.tsx
@@ -1,19 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { reqResApi } from "../api/reqRes";
 import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const paginaRef = useRef(0);
 
   useEffect(() => {
-    reqResApi
-      .get<ReqResListado>("/users")
-      .then((resp) => {
-        console.log(resp.data.data);
-      })
-      .catch((err) => console.error(err));
+    cargarUsuarios()
   }, []);
 
+  const cargarUsuarios =async () => {
+    const resp = await reqResApi.get<ReqResListado>("/users",{
+      params: {
+        page: paginaRef.current
+      }
+    })
+    console.log(resp.data);
+    if(resp.data.data.length > 0) {
+      setUsuarios(resp.data.data);
+      paginaRef.current ++;
+    }else{
+      alert('No hay mas usuarios')
+    }
+  }
+
+  const renderItems = ({last_name, id, first_name,email,avatar}: Usuario) => {
+    return (
+      <tr key={id.toString()}>
+        <th>
+          <img 
+            src={avatar} 
+            alt={first_name} 
+            style={{
+              width: 35, 
+              borderRadius: 100,
+            }}  
+          />
+        </th>
+        <th>{first_name}{last_name}</th>
+        <th>{email}</th>  
+      </tr>
+    )
+  }
   return (
     <>
       <h3>Usuarios</h3>
@@ -25,8 +54,18 @@ export const Usuarios = () => {
             <th>Email</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {
+            usuarios.map(renderItems)
+          }
+        </tbody>
       </table>
+      <button
+        className="btn btn-primary"
+        onClick={cargarUsuarios}
+      >
+          Siguientes
+      </button>
     </>
   );
 };
